be6946993493d6c81296c238f1955d50317bfcd1
Matt <mattf@ilogcorp.com>
2023-06-17 14:53:33 -0400

    version update, gradle build update

--- src/main/java/nitezh/ministock/utils/VersionTools.java
+++ src/main/java/nitezh/ministock/utils/VersionTools.java
@@ -27,7 +27,7 @@ package nitezh.ministock.utils;
 
 public class VersionTools {
 
-    public static final String BUILD = "81";
+    public static final String BUILD = "82";
 
     private VersionTools() {
     }
